cf19707cdfd87a656aa6142926a269fcb92187fb
Hank Kapka <hankkapka@gmail.com>
2021-10-20 09:56:22 -0500

    displays current date at the top of the page

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,21 @@ var currentDay = $('#currentDay');
 var container = $('#container');
 
 
-currentDay = moment().format('MMMM do YYYY');
+var today = moment().format('dddd MMMM do YYYY');
 
-console.log(currentDay);
+
+console.log(today);
+
+
+//testing...1.2.3
+//var today = document.createElement('div');
+// function displayToday(currentDay); {
+// }
+
+
+//displays the current day at the top of the page
+$(currentDay).text(today);
+
+
+//create div in the container to display rows of data
+//$(container).
