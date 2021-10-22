4e1405d5e06232d4512e08cf0f718b1a502942e4
Hank Kapka <hankkapka@gmail.com>
2021-10-21 22:31:58 -0500

    added time blocks and slots to enter tasks

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,54 @@
 var currentDay = $('#currentDay');
-var container = $('#container');
+var container = $('.container');
 
 
+//create today
 var today = moment().format('dddd MMMM do YYYY');
-
-
-console.log(today);
-
-
-//testing...1.2.3
-//var today = document.createElement('div');
-// function displayToday(currentDay); {
-// }
-
 
 //displays the current day at the top of the page
 $(currentDay).text(today);
 
 
-//create div in the container to display rows of data
-//$(container).
+//create an array of the times 
+const scheduleList = [
+    {time: "08:00"},
+    {time: "09:00"},
+    {time: "10:00"},
+    {time: "11:00"},
+    {time: "12:00"},
+    {time: "13:00"},
+    {time: "14:00"},
+    {time: "15:00"},
+    {time: "16:00"},
+ ];
+
+
+
+const buildCalendar = (scheduleList) => {
+    scheduleList?.map((hourBlock) => {
+        
+        const row = document.createElement('li');
+        const timeDisplay = document.createElement('div');
+        const taskDisplay = document.createElement('input');
+        const saveBtn = document.createElement('btn');
+        
+        timeDisplay.textContent = hourBlock?.time;
+        saveBtn.classList.add("saveBtn");
+        row.classList.add("row");
+
+        row.appendChild(timeDisplay);
+        row.appendChild(taskDisplay);
+
+        container.append(row);
+        return row;
+
+    })
+
+};
+
+buildCalendar(scheduleList);
+
+
+
+
+
