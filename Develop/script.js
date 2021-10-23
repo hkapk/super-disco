1026463b4d9138d5700f19e06f7f2dce5606ab00
Hank Kapka <hankkapka@gmail.com>
2021-10-23 10:06:08 -0500

    formatted hour block, text block and save block in CSS

--- Develop/script.js
+++ Develop/script.js
@@ -11,43 +11,66 @@ $(currentDay).text(today);
 
 //create an array of the times 
 const scheduleList = [
-    {time: "08:00"},
-    {time: "09:00"},
-    {time: "10:00"},
-    {time: "11:00"},
-    {time: "12:00"},
-    {time: "13:00"},
-    {time: "14:00"},
-    {time: "15:00"},
-    {time: "16:00"},
+    {time: "9AM"},
+    {time: "10AM"},
+    {time: "11AM"},
+    {time: "12PM"},
+    {time: "1PM"},
+    {time: "2PM"},
+    {time: "3PM"},
+    {time: "4PM"},
+    {time: "5PM"},
  ];
-
 
 
 const buildCalendar = (scheduleList) => {
     scheduleList?.map((hourBlock) => {
         
-        const row = document.createElement('li');
+        const row = document.createElement('div');
         const timeDisplay = document.createElement('div');
         const taskDisplay = document.createElement('input');
         const saveBtn = document.createElement('btn');
         
         timeDisplay.textContent = hourBlock?.time;
-        saveBtn.classList.add("saveBtn");
+        timeDisplay.classList.add("hour");
+        taskDisplay.classList.add("time-block");
         row.classList.add("row");
+        saveBtn.classList.add("saveBtn");
+
 
         row.appendChild(timeDisplay);
         row.appendChild(taskDisplay);
+        row.appendChild(saveBtn);
+
+        console.log(saveBtn);
+
 
         container.append(row);
         return row;
-
+        //save scheduleList task display items to localStorage
+        //localStorage.setItem("taskDisplay",JSON.stringify(taskDisplay));
     })
 
 };
 
 buildCalendar(scheduleList);
 
+//save events when you click the save button
+const saveTasks = function () {
+    saveBtn.addEventListener("click", function() {
+        
+    })
+}
+
+
+//create function to change color of hourBlock based on past/present/future
+// const applyColor = (scheduleList) => {
+//}
+
+// $(document).ready(function () {
+//     $('#btn').addClass('saveBtn');
+
+// });
 
 
 
